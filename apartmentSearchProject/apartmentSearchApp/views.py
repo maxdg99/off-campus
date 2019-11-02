a82d9a3edac3eb9f6c090118d2ece2b5cff5aafc
Adam Lis <adambricelis@gmail.com>
2019-11-02 12:18:14 -0400

    Added a couple sample webpages

--- apartmentSearchProject/apartmentSearchApp/views.py
+++ apartmentSearchProject/apartmentSearchApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world. Let's get this bread my dudes!")
+
+def banana(request):
+    return HttpResponse('<b>Yeehaw</b><img src="https://yt3.ggpht.com/a/AGF-l786UWF6pmeRZI5z8V1lFYr4MFI2RQhyh24vZA=s900-c-k-c0xffffffff-no-rj-mo">')
