c4c18016f5b4238960a25a3239fd73c2ec25c2bd
Adam Lis <adambricelis@gmail.com>
2019-11-02 13:38:23 -0400

    Updated models

--- apartmentSearchProject/apartmentSearchApp/models.py
+++ apartmentSearchProject/apartmentSearchApp/models.py
@@ -2,25 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-class Address(models.Model):
-    line_1 = models.CharField(max_length=100)
-    line_2 = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    zip_code = models.PositiveIntegerField()
+# class Address(models.Model):
+#     line_1 = models.CharField(max_length=100)
+#     line_2 = models.CharField(max_length=100, null=True)
+#     city = models.CharField(max_length=100, default='Columbus')
+#     state = models.CharField(max_length=100, default='Ohio')
+#     country = models.CharField(max_length=100, default='United States')
+#     zip_code = models.PositiveIntegerField()
 
 class Listing(models.Model):
-    AREA_CHOICES = [("N", "North Campus"), ("S", "South Campus")]
+    AREA_CHOICES = [('N', 'North Campus'), ('S', 'South Campus')]
 
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(null=True)
+    # address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
     num_bedrooms = models.PositiveIntegerField()
     num_bathrooms = models.FloatField()
     image_url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
-    miles_from_campus = models.FloatField()
+    miles_from_campus = models.FloatField(null=True)
     area_of_campus = models.CharField(max_length=10, choices=AREA_CHOICES)
     url = models.CharField(max_length=1000)
-    availability_date = models.DateField()
+    availability_date = models.DateField(null=True)
     active = models.BooleanField()
     
