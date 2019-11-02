e5a37d51232df5c62a0ceab073595717e23766d7
Adam Lis <adambricelis@gmail.com>
2019-11-02 18:39:23 -0400

    Minor aesthetic changes

--- apartmentSearchProject/apartmentSearchApp/views.py
+++ apartmentSearchProject/apartmentSearchApp/views.py
@@ -12,10 +12,12 @@ def index(request):
 
 def banana(request):
     l = Listing()
-    l.price = 42069
-    l.address = '42069 W. Lane Ave'
+    l.latitude = 40.006150
+    l.longitude = -83.010930
+    l.price = 69
+    l.address = '420 W Lane Ave'
     l.num_bedrooms = 3
     l.num_bathrooms = 0.5
     l.active = True
     l.save()
-    return HttpResponse('<b>Yeehaw</b><img src="https://yt3.ggpht.com/a/AGF-l786UWF6pmeRZI5z8V1lFYr4MFI2RQhyh24vZA=s900-c-k-c0xffffffff-no-rj-mo">')
+    return HttpResponse('<b>Yeehaw</b><br><img src="https://media1.giphy.com/media/IB9foBA4PVkKA/giphy.gif">')
