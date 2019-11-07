a08a20e389250570e0481b10f2599b45bc1b3a58
Adam Lis <adambricelis@gmail.com>
2019-11-07 15:02:33 -0500

    Added site name and copyright, removed banana

--- apartmentSearchProject/apartmentSearchApp/urls.py
+++ apartmentSearchProject/apartmentSearchApp/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('banana/', views.banana, name='banana'),
     path('query_json', views.query_json, name='query_json')
 ]
--- apartmentSearchProject/apartmentSearchApp/views.py
+++ apartmentSearchProject/apartmentSearchApp/views.py
@@ -82,15 +82,3 @@ def query_json(request):
     results = query_for_request(request)
     
     return HttpResponse(serializers.serialize('json', results), content_type="application/json")
-
-def banana(request):
-    l = Listing()
-    l.latitude = 40.006150
-    l.longitude = -83.010930
-    l.price = 69
-    l.address = '420 W Lane Ave'
-    l.num_bedrooms = 3
-    l.num_bathrooms = 0.5
-    l.active = True
-    l.save()
-    return HttpResponse('<b>Yeehaw</b><br><img src="https://media1.giphy.com/media/IB9foBA4PVkKA/giphy.gif">')
