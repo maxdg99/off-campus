652c33ec8fe28f53c502fa93035e2a63a7242dcc
Adam Lis <adambricelis@gmail.com>
2019-12-05 21:02:04 -0500

    Improved listing card styling

--- off-campus-front-end/src/components/Listing.vue
+++ off-campus-front-end/src/components/Listing.vue
@@ -1,29 +1,66 @@
 <template>
   <div class="uk-card uk-card-small uk-card-default uk-card-hover uk-card-body">
-    <div>
-      <a v-bind:href="listing.url" target="_blank">
-        {{ listing.address }}
-        <span class="open-in-new-tab" uk-icon="icon: arrow-up"></span>
-      </a>
+    <div class="listing-address-parent">
+      <a v-bind:href="listing.url" target="_blank" class="listing-address">{{ listing.address }}</a>
     </div>
     <img v-bind:src="listing.image_url" class="listing-image" />
-    <div class="uk-grid-divider" uk-grid>
-      <div v-if="listing.price">${{ listing.price }}</div>
-      <div v-if="!listing.price">N/A</div>
-      <div>{{ listing.num_bedrooms }} beds</div>
-      <div>{{ listing.num_bathrooms }} baths</div>
-      <div>{{ listing.miles_from_campus }} mi</div>
+    <div class="listing-info-container">
+      <div class="listing-info-row">
+        <div v-if="listing.price" class="listing-info listing-price">${{ listing.price }}</div>
+        <div v-if="!listing.price" class="listing-info">N/A</div>
+        <div class="listing-info">{{ listing.miles_from_campus }} mi</div>
+      </div>
+      <div class="listing-info-row">
+        <div class="listing-info">{{ listing.num_bedrooms }} beds</div>
+        <div class="listing-info">{{ listing.num_bathrooms }} baths</div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.open-in-new-tab {
-    transform: rotateY(0deg) rotate(45deg);
+:root {
+  --card-section-margin: 10px;
+}
+
+.listing-address-parent {
+  text-align: center;
+  margin-bottom: var(--card-section-margin);
+}
+
+.listing-address,
+.listing-address:hover {
+  display: inline-block;
+  color: black;
+  font-size: 1.125em;
 }
 
 .listing-image {
+  display: block;
+  margin: auto;
   height: 200px;
+  margin-bottom: var(--card-section-margin);
+}
+
+.listing-info-container {
+  display: block;
+  margin-left: 20%;
+  margin-right: 10%;
+  font-size: 1.125em;
+}
+
+.listing-info-row {
+  width: 100%;
+}
+
+.listing-info {
+  display: inline-block;
+  width: 50%;
+}
+
+.listing-price {
+  font-weight: bold;
+  color: green;
 }
 </style>
 
