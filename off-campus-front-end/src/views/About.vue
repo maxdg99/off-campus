55246e31d181cae035276fd35a563750a7a12ab2
Maxwell Gruber <maxdg99@gmail.com>
2020-06-04 22:12:45 -0400

    Add to about page.

--- off-campus-front-end/src/views/About.vue
+++ off-campus-front-end/src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h1 class="text-center">What is Off Campus?</h1>
-        <p class="text-center">Off Campus aggregate rental property listings for the Columbus area.</p>
+        <p class="text-center">Off Campus aggregate rental property listings for the Columbus area.<br>Our goal is to help students find the perfect off campus house or apartment in a fraction of the time.</p>
+    
+        
     </div>
 </template>
 
