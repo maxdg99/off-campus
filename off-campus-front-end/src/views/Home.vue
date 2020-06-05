bc74ca000a546188db63800683233f74f0cc8876
Maxwell Gruber <maxdg99@gmail.com>
2020-06-04 20:37:41 -0400

    Move copywrite over background.  Darken background iamge

--- off-campus-front-end/src/views/Home.vue
+++ off-campus-front-end/src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="home">
-    <div class="uk-container uk-text-center uk-background-cover" id="content">
+  <div class="uk-background-cover" id="content">
+    <div class="uk-container uk-text-center">
       <div id="button-text-group" class="uk-position-center">
         <h2 class="welcome-text">Go Bucks, Home Searching Sucks!</h2>
-        <h3 class="welcome-text">Make finding a place easier, with Off Campus.</h3>
+        <h3 class="welcome-text">Make finding a place easier, with Off Campus</h3>
         <router-link to="/search" tag="button" class="uk-button uk-button-primary">Search Properties</router-link>
       </div>
     </div>
-    <br>
-    <footer class="uk-text-center">&copy; {{new Date().getFullYear()}} Max Gruber, Andrew Haberlandt, Adam Lis</footer>
+    <footer id="footer" class="uk-text-center">&copy; {{new Date().getFullYear()}} Max Gruber, Andrew Haberlandt, Adam Lis</footer>
   </div>
 </template>
 
 <style scoped>
 #content {
-  background-image: url("landing_page.jpg");
-  height: 100vh;
+  background-image: 
+    linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ), 
+    url(landing_page.jpg);
+  height: calc(100vh - 82px);
   position: relative;
 }
 
@@ -28,6 +32,13 @@
 #button-text-group {
   position: absolute;
   top: 30%;
+}
+
+#footer {
+  color: white;
+  position: absolute;
+  bottom: 0%;
+  width: 100%;
 }
 </style>
 
