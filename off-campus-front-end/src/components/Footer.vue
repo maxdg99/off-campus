701f3f0b45d9006716991ce95813ef1ddd91f966
Adam Lis <adambricelis@gmail.com>
2020-06-06 22:13:44 -0400

    Added footer component

---
+++ off-campus-front-end/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="uk-text-center">&copy; {{new Date().getFullYear()}} Max Gruber, Andrew Haberlandt, Adam Lis</footer>
+</template>
+
+<style scoped>
+footer {
+  padding: 0.5rem 0;
+}
+</style>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
