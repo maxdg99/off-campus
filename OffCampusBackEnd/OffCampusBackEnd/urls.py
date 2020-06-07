336676efdd7edb95cebaf69083369908b89c89fd
Andrew Haberlandt <haberlandt.3@osu.edu>
2020-06-07 22:28:29 +0000

    remove admin URLs and tweak production uwsgi config

--- OffCampusBackEnd/OffCampusBackEnd/urls.py
+++ OffCampusBackEnd/OffCampusBackEnd/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('OffCampusRestApi.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
