359d066dbc585724114a051766004ff3effbffb3
Adam Lis <adambricelis@gmail.com>
2020-06-06 22:15:22 -0400

    Switched landing page to footer component

--- off-campus-front-end/src/views/Home.vue
+++ off-campus-front-end/src/views/Home.vue
@@ -7,17 +7,13 @@
         <router-link to="/search" tag="button" class="uk-button uk-button-primary">Search Properties</router-link>
       </div>
     </div>
-    <footer id="footer" class="uk-text-center">&copy; {{new Date().getFullYear()}} Max Gruber, Andrew Haberlandt, Adam Lis</footer>
+    <Footer id="footer" />
   </div>
 </template>
 
 <style scoped>
 #content {
-  background-image: 
-    linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-    ), 
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url(landing_page.jpg);
   height: calc(100vh - 82px);
   position: relative;
@@ -44,7 +40,12 @@
 </style>
 
 <script>
+import Footer from "@/components/Footer.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Footer
+  }
 };
 </script>
